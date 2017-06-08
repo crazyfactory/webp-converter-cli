eaba13a4a269e580438f7492e0bc0a9a6b6b7ba5
wmathes <fanatik@gmx.at>
2017-06-08 11:06:24 +0700

    feat(concurrency): Add concurrency and verbose logging

--- bin/index.js
+++ bin/index.js
@@ -2,63 +2,87 @@
 
 const fs = require('fs');
 const execFile = require('child_process').execFile;
-const readChunk = require('read-chunk');
-const imageType = require('image-type');
 const cwebp = require('cwebp-bin');
 const program = require('commander');
 const chalk = require('chalk');
+const throat = require('throat');
 
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
 
+const findImages = require('../src/find-images');
+
+// Respect semantic-release
+pkg.version = pkg.version || 'dev';
 updateNotifier({pkg}).notify();
 
-let fileList = [];
+// Define commander
+program
+    .version(pkg.version || 'dev')
+    .option('-r, --recursive', 'Walk given directory recursively')
+    .option('-p, --path <path>', 'A path to crawl, defaults to current working directory')
+    .option('-f, --files <items>', 'the files you want to convert,split by \',\'', v => v.split(','))
+    .option('-c, --concurrency <number>', 'Parallel compression tasks, default 4', parseInt)
+    .option('-v, --verbose', 'display additional information')
+    .parse(process.argv);
 
+// Determine Files
+const files = program.files
+    ? program.files
+    : findImages(process.path || process.cwd(), ['png', 'jpg'], program.recursive);
+console.log(chalk.yellow('Found ' + files.length + ' image file(s)!'));
+
+// Create Queue
+const concurrency = program.concurrency || 4;
+const t = throat(concurrency);
+
+// Helper func
 function queryExtName(fileName) {
     return /\.[^.]+$/.exec(fileName);
 }
 
-function walk(path) {
-    const dirList = fs.readdirSync(path);
-    dirList.forEach(item => {
-        if (fs.statSync(path + '/' + item).isFile()) {
-            const imageTypeData = imageType(readChunk.sync(path + '/' + item, 0, 12));
-            if (imageTypeData) {
-                if (imageTypeData.ext === 'jpg' || imageTypeData.ext === 'png') {
-                    fileList.push(path + '/' + item);
-                }
-            }
-        }
-    });
+// Fill Queue
+let totalSize = 0;
+let totalSaved = 0;
+let convertedCount = 0;
+const jobs = files.map(file => file && t(() =>
+    new Promise(resolve => {
+        const output = file.replace(queryExtName(file), '.webp');
+        const size = fs.statSync(file).size;
+        totalSize += size;
 
-    if (program.recursive) {
-        dirList.forEach(item => {
-            if (fs.statSync(path + '/' + item).isDirectory()) {
-                walk(path + '/' + item);
+        // Begin
+        if (program.verbose) {
+            console.log(chalk.gray(file + ' is being converted'));
+        }
+        execFile(cwebp, [file, '-o', output], err => {
+            if (err) {
+                if (program.verbose) {
+                    console.log(chalk.gray(err));
+                }
+                console.log(chalk.red(file + ' failed with error!'));
+                resolve();
+                return;
             }
+
+            const newSize = fs.statSync(output).size;
+            const percDiff = Math.round((1 - (newSize / size)) * 100);
+
+            console.log(chalk.green(file + ' converted, saving ' + (size - newSize) + ' bytes (' + percDiff + '%)'));
+            totalSaved += size - newSize;
+            convertedCount++;
+            resolve();
         });
     }
-}
+)));
 
-program
-    .version(pkg.version || 'dev')
-    .option('-r, --recursive', 'Walk given directory recursively')
-    .option('-f, --files <items>', 'the files you want to convert,split by \',\'', v => v.split(','))
-    .parse(process.argv);
+// Report
+Promise.all(jobs).then(err => {
+    const failCount = files.length - convertedCount;
+    if (err || failCount > 0) {
+        console.log(chalk.red('Could not process ' + failCount + ' file(s).'));
+    }
 
-if (program.files) {
-    fileList = program.files;
-} else {
-    walk(process.cwd());
-}
-
-console.log(chalk.yellow('Found ' + fileList.length + ' image file(s) !'));
-fileList.forEach(file => {
-    execFile(cwebp, [file, '-o', file.replace(queryExtName(file), '.webp')], err => {
-        if (err) {
-            throw err;
-        }
-        console.log(chalk.green(file + ' is converted!'));
-    });
+    const percDiff = Math.round((totalSaved / totalSize) * 100);
+    console.log(chalk.yellow('Converted ' + convertedCount + ' file(s), saving ' + totalSaved + ' bytes (' + percDiff + '%)'));
 });
