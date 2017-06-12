e3997b3242674d15313fb6d1e3f73238c77afd47
wmathes <fanatik@gmx.at>
2017-06-12 15:14:46 +0700

    feat(skip): Add --skip, refactor --output and make output dir on demand.

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+const path = require('path');
 const execFile = require('child_process').execFile;
+const fs = require('fs-extra');
 const cwebp = require('cwebp-bin');
 const program = require('commander');
 const chalk = require('chalk');
@@ -10,44 +11,71 @@ const throat = require('throat');
 const updateNotifier = require('update-notifier');
 const pkg = require('../package.json');
 
-const findImages = require('../src/find-images');
+const findImages = require('../src/internals').findImages;
+const mapOutput = require('../src/internals').mapOutput;
+const outputExists = require('../src/internals').outputExists;
 
-// Respect semantic-release
-pkg.version = pkg.version || 'dev';
-updateNotifier({pkg}).notify();
+// Notify for updates
+if (pkg.version) {
+    updateNotifier({pkg}).notify();
+}
 
 // Define commander
 program
     .version(pkg.version || 'dev')
     .option('-r, --recursive', 'Walk given directory recursively')
     .option('-p, --path <path>', 'A path to crawl, defaults to current working directory')
+    .option('-o, --output <path>', 'A target path to output the converted files, when using --files, provide multiple and separate by comma', v => v.split(','))
     .option('-f, --files <items>', 'the files you want to convert,split by \',\'', v => v.split(','))
+    .option('-s, --skip', 'Will skip files if their output file already exists')
     .option('-c, --concurrency <number>', 'Parallel compression tasks, default 4', parseInt)
     .option('-v, --verbose', 'display additional information')
     .parse(process.argv);
 
-// Determine Files
-const files = program.files
-    ? program.files
-    : findImages(process.path || process.cwd(), ['png', 'jpg'], program.recursive);
+// Running in Path or File Mode?
+const pathMode = !program.files || !program.files.length;
+if (!pathMode && program.output && program.output.length > 0 && program.output.length !== program.files.length) {
+    console.log(chalk.red('expected 0 or ' + program.files.length + ' values for output-parameter, found ' + program.output.length));
+    process.exit(1);
+}
+
+// Determine files
+const files = program.files || findImages(process.path || process.cwd(), ['png', 'jpg'], program.recursive);
 console.log(chalk.yellow('Found ' + files.length + ' image file(s)!'));
 
+// Determine pathMode values
+const basePath = pathMode && (process.path || process.cwd());
+const outputPath = pathMode && ((program.output && program.output[0]) || basePath);
+
 // Create Queue
-const concurrency = program.concurrency || 4;
-const t = throat(concurrency);
+const t = throat(program.concurrency || 4);
 
 // Helper func
-function queryExtName(fileName) {
-    return /\.[^.]+$/.exec(fileName);
+function replaceExt(fileName, ext = '.webp') {
+    return fileName && fileName.replace(/\.[^.]+$/.exec(fileName), ext);
 }
 
 // Fill Queue
 let totalSize = 0;
 let totalSaved = 0;
+let totalSkipped = 0;
 let convertedCount = 0;
-const jobs = files.map(file => file && t(() =>
+const jobs = files.map((file, index) => file && t(() =>
     new Promise(resolve => {
-        const output = file.replace(queryExtName(file), '.webp');
+        // Determine the output name
+        const output = replaceExt(mapOutput(file, basePath, outputPath, program.output && program.output[index]));
+        fs.ensureDirSync(path.dirname(output));
+
+        // Skip?
+        if (program.skip && outputExists(output)) {
+            totalSkipped++;
+            if (program.verbose) {
+                console.log(chalk.gray(file + ' skipped.'));
+            }
+            resolve();
+            return;
+        }
+
         const size = fs.statSync(file).size;
         totalSize += size;
 
@@ -78,11 +106,15 @@ const jobs = files.map(file => file && t(() =>
 
 // Report
 Promise.all(jobs).then(() => {
-    const failCount = files.length - convertedCount;
+    if (program.verbose && totalSkipped > 0) {
+        console.log(chalk.gray('Skipped ' + totalSkipped + ' file(s).'));
+    }
+
+    const failCount = files.length - convertedCount - totalSkipped;
     if (failCount > 0) {
         console.log(chalk.red('Could not process ' + failCount + ' file(s).'));
     }
 
-    const percDiff = Math.round((totalSaved / totalSize) * 100);
+    const percDiff = Math.round((totalSaved / totalSize) * 100) || 0;
     console.log(chalk.yellow('Converted ' + convertedCount + ' file(s), saving ' + totalSaved + ' bytes (' + percDiff + '%)'));
 });
